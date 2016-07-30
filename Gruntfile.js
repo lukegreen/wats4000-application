d7df787eaf0549fe29bacfb238316939370801f7
Luke Green <elukuent@gmail.com>
2016-07-29 22:25:43 -0700

    fixed gruntfile
    added repo ssh to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:lukegreen/wats4000-application.git',
           branch: 'gh-pages'
         }
       }
