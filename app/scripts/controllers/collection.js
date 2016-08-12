95d61fc5b05c8a21c828dce52db704ef9272ebf0
Luke Green <elukuent@gmail.com>
2016-08-11 23:05:36 -0700

    got localStorage script working
    I think...

--- app/scripts/controllers/collection.js
+++ app/scripts/controllers/collection.js
@@ -8,18 +8,38 @@
  * Controller of the wats4000ApplicationApp
  */
 angular.module('wats4000ApplicationApp')
-  .controller('CollectionCtrl', function ($scope, $routeParams, collection) {
+  .controller('CollectionCtrl', function ($scope, $routeParams, collection, $localStorage) {
 
-    /*$scope.username = "Agnoxis";
-
-      $scope.collection = collection.query({
-        username: $scope.username
-      });*/
-
-      $scope.findCollection = function(){
+    //This function finds a collection based in the username in the view input
+    $scope.findCollection = function(){
         $scope.collection = collection.query({
           username: $scope.username
-        })
-      }
+        });
+      };
 
+      //Here we'll setup localStorage for usernames we want to keep
+      $scope.saveUser = function(){
+          var thisUser = $scope.username;
+          console.log(thisUser);
+
+    if (!$localStorage.savedUsers){
+        $localStorage.savedUsers = [thisUser];
+    } else {
+        // We have already saved some cities.
+        // Check to make sure we haven't already saved the current city.
+        var save = true; // Initialize the save decision variable.
+        // Use this loop to check if we've already saved the city.
+        for (var i=0; i < $localStorage.savedUsers.length; i++){
+            if ($localStorage.savedUsers[i] === thisUser) {
+                // This is a duplicate, so don't save (variable set to false).
+                save = false;
+            }
+        }
+        if (save===true){
+            $localStorage.savedUsers.push(thisUser);
+        } else {
+            console.log('this username is already saved');
+        }
+      }
+    };
   });
