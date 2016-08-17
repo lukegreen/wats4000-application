cb0369f4414c4ff37530e94fa6b6acc38b50a48e
Luke Green <elukuent@gmail.com>
2016-08-17 09:48:35 -0700

    still setting up saved collections and messages
    also enhanced styling

--- app/scripts/controllers/collection.js
+++ app/scripts/controllers/collection.js
@@ -21,37 +21,64 @@ angular.module('wats4000ApplicationApp')
       $scope.saveUser = function(){
           var thisUser = $scope.username;
           var thisCollection = $scope.collection;
+          var userObject = {
+            user: null,
+            games: []
+          };
 
-          thisCollection.username = thisUser;
+          //set the user property in userObject
+        	userObject.user = thisUser;
 
-          console.log(thisUser);
-          console.log(thisCollection);
+          var objectCheckOne = userObject.user;
+          console.log(objectCheckOne);
 
+          //add game names to userObject
+          for (var x=0; x < thisCollection.length; ++x) {
+          	var gameName = thisCollection[x].name;
+          	userObject.games.push(gameName);
+          };
 
-    if (!$localStorage.savedUsers){
-        $localStorage.savedUsers = [thisUser];
-        $localStorage.savedCollections = [thisCollection];
-    } else {
-        // We have already saved some cities.
-        // Check to make sure we haven't already saved the current city.
-        var save = true; // Initialize the save decision variable.
-        // Use this loop to check if we've already saved the city.
-        for (var i=0; i < $localStorage.savedUsers.length; i++){
-            if ($localStorage.savedUsers[i] === thisUser) {
-                // This is a duplicate, so don't save (variable set to false).
-                save = false;
+          var objectCheckTwo = userObject.games[0];
+          console.log(objectCheckTwo);
+
+          if (!$localStorage.savedUsers){
+              $localStorage.savedUsers = [thisUser];
+              $localStorage.savedCollections = [userObject];
+          } else {
+              // We have already saved a user.
+              // Check to make sure we haven't already saved the current user.
+              var save = true; // Initialize the save decision variable.
+              // Use this loop to check if we've already saved the user.
+              for (var i=0; i < $localStorage.savedUsers.length; i++){
+                  if ($localStorage.savedUsers[i] === thisUser) {
+                      // This is a duplicate, so don't save (variable set to false).
+                      save = false;
+                  }
+              }
+              if (save===true){
+                  $localStorage.savedUsers.push(thisUser);
+                  $localStorage.savedCollections.push(userObject);
+                  // Add object to trigger messages
+                  $scope.collectionSaved = {
+                    'success': true
+                  };
+              } else {
+                  console.log('this username is already saved');
+
+                  // Add object to trigger messages
+                   $scope.collectionSaved = {
+                       'duplicate': true
+                   };
+              }
             }
-        }
-        if (save===true){
-            $localStorage.savedUsers.push(thisUser);
-            $localStorage.savedCollections.push(thisCollection);
-        } else {
-            console.log('this username is already saved');
-        }
-      }
-    };
+          };
 
     //define storage variable for use in the collections view
     $scope.storage = $localStorage;
+
+    /*$scope.displayCollection = function() {
+      var collectionVisible = $localStorage.savedCollections.games;
+      console.log(collectionVisible);
+    }*/
 
   });
